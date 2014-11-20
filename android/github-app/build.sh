0fa5343c8517b333803c08d8fba3dd5ee614bdd5
Simon Allier <simon.allier@gmail.com>
2014-11-20 15:16:17 +0100

    fix

--- android/github-app/build.sh
+++ android/github-app/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm integration-tests/src/main/java/com/github/mobile/tests/issue/EditIssueActivityTest.java
 mvn test
-mvn clean android:emulator-start -Dandroid.emulator.avd=myandroid -Dandroid.emulator.options="-no-window -no-audio" -Dandroid.emulator.wait=100000 install
-mvn android:emulator-stop
-mvn clean android:emulator-start -Dandroid.emulator.avd=myandroid -Dandroid.emulator.options="-no-window -no-audio" -Dandroid.emulator.wait=100000 install
-mvn android:emulator-stop
+mvn clean android:emulator-start -Dandroid.emulator.avd=myandroid -Dandroid.emulator.options="-no-window -no-audio -wipe-data" -Dandroid.emulator.wait=100000 install
+mvn android:emulator-stop-all
+mvn clean android:emulator-start -Dandroid.emulator.avd=myandroid -Dandroid.emulator.options="-no-window -no-audio -wipe-data" -Dandroid.emulator.wait=100000 install
+mvn android:emulator-stop-all
